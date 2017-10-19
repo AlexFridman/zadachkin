f32ef79ca81b6b3f54b5c42b5904e9d8ee29c7c9
alexfridman <alexfridman@outlook.com>
2017-10-19 23:07:48 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='zadachkin',
+    version='',
+    packages=['zadachkin', 'zadachkin.db'],
+    url='',
+    license='',
+    author='alexfridman',
+    author_email='alexfridman@outlook.com',
+    description=''
+)
